4377891f15fe775c116532f187437cc769ed0850
Michael Crismali <michael.crismali@gmail.com>
2014-02-02 17:58:30 -0600

    got first test passing; added ability to skip layout

--- app/controllers/magic_carpet/js_fixtures_controller.rb
+++ app/controllers/magic_carpet/js_fixtures_controller.rb
@@ -2,10 +2,13 @@ require_dependency "magic_carpet/application_controller"
 
 module MagicCarpet
   class JsFixturesController < ApplicationController
+
     def index
-      # require "pry";binding.pry
-      # main_app.scope.view_renderer.render(self, template: "#{params[:fixture_controller]}/#{params[:fixture_action]}")
-      render template: "wishes/plain"
+      controller = self.class.const_get("#{params[:fixture_controller].capitalize}Controller")
+      options = { }
+      options[:layout] = params[:layout] if params.has_key?(:layout)
+      content = controller.new.render_to_string "#{params[:fixture_controller]}/#{params[:fixture_action]}", options
+      render text: content
     end
   end
 end
--- spec/controllers/magic_carpet/js_fixtures_controller_spec.rb
+++ spec/controllers/magic_carpet/js_fixtures_controller_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 module MagicCarpet
   describe JsFixturesController do
+
     describe "fetching a view without variables" do
       it "returns the view's markup" do
         get :index, fixture_controller: "wishes", fixture_action: "plain", use_route: :magic_carpet
-        expect(response.body).to eq("<h1>Plain</h1>")
+        expect(response.body).to match("<h1>Plain</h1>\n")
+        expect(response.body).to match("<!DOCTYPE html>")
+        expect(response.body).to match("<head>")
+      end
+
+      it "returns the view's markup without the layout" do
+        get :index, layout: false, fixture_controller: "wishes", fixture_action: "plain", use_route: :magic_carpet
+        expect(response.body).to match("<h1>Plain</h1>\n")
+        expect(response.body).to_not match("<!DOCTYPE html>")
+        expect(response.body).to_not match("<head>")
       end
     end
   end
