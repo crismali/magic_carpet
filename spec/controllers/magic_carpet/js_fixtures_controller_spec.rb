2daa7334437e56b2df1fb9196300d5128627b235
Michael Crismali <michael.crismali@gmail.com>
2014-02-02 21:12:46 -0600

    added specs around specifying a layout

--- spec/controllers/magic_carpet/js_fixtures_controller_spec.rb
+++ spec/controllers/magic_carpet/js_fixtures_controller_spec.rb
@@ -8,14 +8,26 @@ module MagicCarpet
         get :index, fixture_controller: "wishes", fixture_action: "plain", use_route: :magic_carpet
         expect(response.body).to match("<h1>Plain</h1>\n")
         expect(response.body).to match("<!DOCTYPE html>")
-        expect(response.body).to match("<head>")
+        expect(response.body).to match("<h1>Application Layout</h1>")
       end
+    end
+
+    describe "fetching a view with local variables"
+
+    describe "fetching a view with instance variables"
+
+    describe "fetching a view with a specified layout" do
 
       it "returns the view's markup without the layout" do
         get :index, layout: false, fixture_controller: "wishes", fixture_action: "plain", use_route: :magic_carpet
-        expect(response.body).to match("<h1>Plain</h1>\n")
+        expect(response.body).to eq("<h1>Plain</h1>\n")
         expect(response.body).to_not match("<!DOCTYPE html>")
-        expect(response.body).to_not match("<head>")
+      end
+
+      it "returns the view's markup with the specified layout" do
+        get :index, layout: "other_layout", fixture_controller: "wishes", fixture_action: "plain", use_route: :magic_carpet
+        expect(response.body).to match("<h1>Other Layout</h1>")
+        expect(response.body).to_not match("<h1>Application Layout</h1>")
       end
     end
   end
